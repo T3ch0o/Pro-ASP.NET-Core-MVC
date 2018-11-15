554d2390f296298f6f7f16f86a45288c96c3a368
T3ch0o <trottimusbg@gmail.com>
2018-11-15 15:38:56 +0200

    [Product] Added Related property.

--- LanguageFuteres/LanguageFuteres/Models/Product.cs
+++ LanguageFuteres/LanguageFuteres/Models/Product.cs
@@ -4,12 +4,16 @@
     {
         public string Name { get; set; }
 
+        public Product Related { get; set; }
+
         public decimal? Price { get; set; }
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product { Name = "Kayak", Price = 275M };
             Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+
+            kayak.Related = lifejacket;
             return new [] { kayak, lifejacket, null };
         }
     }
