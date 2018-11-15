ddf845b3f94db41a9cd5269eee8a4a614d2748a2
T3ch0o <trottimusbg@gmail.com>
2018-11-15 15:39:48 +0200

    [HomeController] Index action GetProducts and passes them to to View.

--- LanguageFuteres/LanguageFuteres/Controllers/HomeController.cs
+++ LanguageFuteres/LanguageFuteres/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 ﻿namespace LanguageFeatures.Controllers
 {
+    using System.Collections.Generic;
+
+    using LanguageFeatures.Models;
+
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            return View(new [] { "C#", "Language", "Features" });
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
+            }
+
+            return View(results);
         }
     }
 }
