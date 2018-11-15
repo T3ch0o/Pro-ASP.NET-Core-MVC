b7cb2e36678d1e3d8b02de7bd157a51fc814a4fc
T3ch0o <trottimusbg@gmail.com>
2018-11-15 19:55:31 +0200

    [ShoppingCart] Added model.

---
+++ LanguageFuteres/LanguageFuteres/Models/ShoppingCart.cs
@@ -0,0 +1,9 @@
+﻿namespace LanguageFeatures.Models
+{
+    using System.Collections.Generic;
+
+    public class ShoppingCart
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
