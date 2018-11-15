fef9d8c765da8d82c1cd08935cfe1bcdc23c5279
T3ch0o <trottimusbg@gmail.com>
2018-11-15 15:23:26 +0200

    [HomeController] Created index action and passes array.

---
+++ LanguageFuteres/LanguageFuteres/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace LanguageFeatures.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(new [] { "C#", "Language", "Features" });
+        }
+    }
+}
