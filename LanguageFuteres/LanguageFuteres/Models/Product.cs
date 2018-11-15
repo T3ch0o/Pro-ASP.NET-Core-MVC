417dbee6f424234287b26589f7b32c3f20deb8b4
T3ch0o <trottimusbg@gmail.com>
2018-11-15 19:30:09 +0200

    [Product] Added Category property.

--- LanguageFuteres/LanguageFuteres/Models/Product.cs
+++ LanguageFuteres/LanguageFuteres/Models/Product.cs
@@ -4,14 +4,26 @@
     {
         public string Name { get; set; }
 
+        public string Category { get; set; } = "Water Sports";
+
         public Product Related { get; set; }
 
         public decimal? Price { get; set; }
 
         public static Product[] GetProducts()
         {
-            Product kayak = new Product { Name = "Kayak", Price = 275M };
-            Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Category = "Water Craft",
+                Price = 275M
+            };
+
+            Product lifejacket = new Product
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
 
             kayak.Related = lifejacket;
             return new [] { kayak, lifejacket, null };
