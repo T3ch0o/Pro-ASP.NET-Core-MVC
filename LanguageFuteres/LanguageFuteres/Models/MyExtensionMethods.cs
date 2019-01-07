400ca497dd5dcead232bb978e5b50ffd0d319ec3
Techo <trottimusbg@gmail.com>
2019-01-07 09:29:43 +0200

    [MyExtensionMethod] Add FilterByPrice method wich filter by minimum price.

--- LanguageFuteres/LanguageFuteres/Models/MyExtensionMethods.cs
+++ LanguageFuteres/LanguageFuteres/Models/MyExtensionMethods.cs
@@ -1,16 +1,30 @@
 ﻿namespace LanguageFeatures.Models
 {
+    using System.Collections.Generic;
+
     public static class MyExtensionMethods
     {
-        public static decimal TotalPrices(this ShoppingCart cartParam)
+        public static decimal TotalPrices(this IEnumerable<Product> produts)
         {
             decimal total = 0;
-            foreach (Product prod in cartParam.Products)
+
+            foreach (Product product in produts)
             {
-                total += prod?.Price ?? 0;
+                total += product?.Price ?? 0;
             }
 
             return total;
+        }
+
+        public static IEnumerable<Product> FilterByPrice(this IEnumerable<Product> productEnum, decimal minimumPrice)
+        {
+            foreach (Product product in productEnum)
+            {
+                if ((product?.Price ?? 0) >= minimumPrice)
+                {
+                    yield return product;
+                }
+            }
         }
     }
 }
