48f862c9cc9e9779ef2a3bd2db092106a997c866
Techo <trottimusbg@gmail.com>
2019-01-08 14:57:54 +0200

    [MyAsyncMethods] Add GetPathLengthAsync method wich gets content length of apress.com.

---
+++ LanguageFuteres/LanguageFuteres/Models/MyAsyncMethods.cs
@@ -0,0 +1,17 @@
+﻿namespace LanguageFeatures.Models
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class MyAsyncMethods
+    {
+        public static async Task<long?> GetPathLengthAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage httpMessage = await client.GetAsync("http://apress.com");
+
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
