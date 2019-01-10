43bbe81558992aea585665e78e721dd50dc11932
T3ch0o <trottimusbg@gmail.com>
2019-01-10 22:09:01 +0200

    [ProductController] Add List action.

---
+++ Sports Store/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsStore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Services.Interfaces;
+
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ViewResult List()
+        {
+            return View(_productService.GetAll);
+        }
+    }
+}
