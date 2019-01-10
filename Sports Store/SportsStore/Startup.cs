3df03c73c7840adae79121cd3da7da93a720c769
T3ch0o <trottimusbg@gmail.com>
2019-01-10 20:52:20 +0200

    [SportsStore] Initial comit.

---
+++ Sports Store/SportsStore/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace SportsStore
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+
+            });
+        }
+    }
+}
