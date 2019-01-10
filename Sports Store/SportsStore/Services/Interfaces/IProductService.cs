1206db582d38a3370f14e24704bc4f684fd24ddf
T3ch0o <trottimusbg@gmail.com>
2019-01-10 22:08:22 +0200

    [ProductService] Add GetAll property with List of Product.

---
+++ Sports Store/SportsStore/Services/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStore.Services.Interfaces
+{
+    using System.Linq;
+
+    using SportsStore.Models;
+
+    public interface IProductService
+    {
+        IQueryable<Product> GetAll { get; }
+    }
+}
---
+++ Sports Store/SportsStore/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿namespace SportsStore.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SportsStore.Models;
+    using SportsStore.Services.Interfaces;
+
+    public class ProductService : IProductService
+    {
+        public IQueryable<Product> GetAll => new List<Product>
+        {
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Surf board", Price = 179 },
+            new Product { Name = "Running shoes", Price = 95 }
+        }.AsQueryable();
+    }
+}
