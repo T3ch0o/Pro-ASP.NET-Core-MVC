d323eb9020d6bdf71a66a17ddefde621080db639
T3ch0o <trottimusbg@gmail.com>
2019-01-11 22:40:36 +0200

    [MiddlewareExtension] Add UseSeedData middleware.

---
+++ Sports Store/SportsStore/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsStore.Middlewares
+{
+    using System;
+
+    using Microsoft.AspNetCore.Builder;
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSeedData(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SeedDataMiddleware>();
+        }
+    }
+}
