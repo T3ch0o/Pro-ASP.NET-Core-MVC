e303e890d740861050c87e493d59c3e58d672e2d
T3ch0o <trottimusbg@gmail.com>
2019-01-14 15:51:59 +0200

    [CartLine] Hold Product and Quantity.

---
+++ Sports Store/SportsStore/Models/CartLine.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsStore.Models
+{
+    public class CartLine : BaseModel<int>
+    {
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
