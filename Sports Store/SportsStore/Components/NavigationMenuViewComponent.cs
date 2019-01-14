e294e05203ed6712c4628e55384abdbc878eb76d
T3ch0o <trottimusbg@gmail.com>
2019-01-14 14:23:06 +0200

    [NavigationMenuViewComponent] Add navigation component for the application.

---
+++ Sports Store/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,31 @@
+﻿namespace SportsStore.Components
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Services.Interfaces;
+
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public NavigationMenuViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+
+            IEnumerable<string> productsCategory = _productService.GetAll()
+                                                                    .Select(p => p.Category)
+                                                                    .Distinct()
+                                                                    .OrderBy(p => p);
+
+            return View(productsCategory);
+        }
+    }
+}
