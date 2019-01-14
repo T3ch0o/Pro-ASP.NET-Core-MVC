6a83453abcf39175244db7436e3fdb4f838d914b
T3ch0o <trottimusbg@gmail.com>
2019-01-14 23:53:01 +0200

    [CartController] Add actions to add and remove item from the cart.

---
+++ Sports Store/SportsStore/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿namespace SportsStore.Controllers
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models;
+    using SportsStore.Models.ViewModels;
+    using SportsStore.Services.Interfaces;
+
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+
+        private readonly Cart _cart;
+
+        public IActionResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = _cart,
+                ReturnUrl = returnUrl,
+                HasItems = _cart.CartLines.Any()
+            });
+        }
+
+        public CartController(IProductService productService, Cart cart)
+        {
+            _productService = productService;
+            _cart = cart;
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = _productService.GetAll().FirstOrDefault(p => p.Id == productId);
+
+            if (product != null)
+            {
+                _cart.AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = _productService.GetAll().FirstOrDefault(p => p.Id == productId);
+
+            if (product != null)
+            {
+                _cart.RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { ReturnUrl = returnUrl });
+        }
+    }
+}
