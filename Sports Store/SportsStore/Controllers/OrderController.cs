670f57aa2e2fbee61a8ea293eba171032c2b9bd6
T3ch0o <trottimusbg@gmail.com>
2019-01-16 21:47:59 +0200

    [OrderController] Add Checkout get and post action.

---
+++ Sports Store/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿namespace SportsStore.Controllers
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models;
+    using SportsStore.Models.ViewModels;
+    using SportsStore.Services.Interfaces;
+
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        private Cart _cart;
+
+        public OrderController(IOrderService orderService, Cart cart)
+        {
+            _orderService = orderService;
+            _cart = cart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (!_cart.CartLines.Any())
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.CartLines = _cart.CartLines.ToArray();
+                _orderService.SaveOrder(order);
+
+                return RedirectToAction(nameof(Completed));
+            }
+
+            return View(order);
+        }
+
+       
+    }
+}
