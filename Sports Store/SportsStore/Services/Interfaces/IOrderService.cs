b501d2fe185be4349c79ab5191f85eb26068f4da
T3ch0o <trottimusbg@gmail.com>
2019-01-16 21:51:30 +0200

    [OrderService] Add GetAll method to return all orders and SaveOrder.

---
+++ Sports Store/SportsStore/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsStore.Services.Interfaces
+{
+    using System.Linq;
+
+    using SportsStore.Models;
+    using SportsStore.Models.ViewModels;
+
+    public interface IOrderService
+    {
+        IQueryable<Order> GetAll();
+
+        void SaveOrder(Order order);
+    }
+}
---
+++ Sports Store/SportsStore/Services/OrderService.cs
@@ -0,0 +1,37 @@
+﻿namespace SportsStore.Services
+{
+    using System.Linq;
+
+    using Microsoft.EntityFrameworkCore;
+
+    using SportsStore.Data;
+    using SportsStore.Models;
+    using SportsStore.Services.Interfaces;
+
+    public class OrderService : IOrderService
+    {
+        private readonly SportsStoreDbContext _db;
+
+        public OrderService(SportsStoreDbContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<Order> GetAll()
+        {
+            return _db.Orders.Include(o => o.CartLines).ThenInclude(c => c.Product);
+        }
+
+        public void SaveOrder(Order order)
+        {
+            _db.AttachRange(order.CartLines.Select(c => c.Product));
+
+            if (order.Id == 0)
+            {
+                _db.Orders.Add(order);
+            }
+
+            _db.SaveChanges();
+        }
+    }
+}
