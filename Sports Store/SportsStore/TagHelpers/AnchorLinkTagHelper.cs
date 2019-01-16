82e432081973eafdf84d02c720f831f2b3a089d7
T3ch0o <trottimusbg@gmail.com>
2019-01-16 21:53:37 +0200

    [AnchorLinkTagHelper] Add tag helper for anchor tag to encode url query.

---
+++ Sports Store/SportsStore/TagHelpers/AnchorLinkTagHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace SportsStore.TagHelpers
+{
+    using System.Web;
+
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    [HtmlTargetElement("a", Attributes = "page-controller")]
+    public class AnchorLinkTagHelper : TagHelper
+    {
+        public string PageArea { get; set; }
+
+        public string PageController { get; set; }
+
+        public string PageAction { get; set; }
+
+        public string PageReturnUrl { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            if (!PageReturnUrl.Contains('%'))
+            {
+                PageReturnUrl = HttpUtility.UrlEncode(PageReturnUrl);
+            }
+
+            output.Attributes.Add("href", $"/{PageController}/{PageAction}?returnUrl={PageReturnUrl}");
+        }
+    }
+}
