e595fc00c4331302f8ae4ed3ed359887d4899c7a
T3ch0o <trottimusbg@gmail.com>
2019-01-16 21:50:23 +0200

    [BaseModel] Add BindNever attribute.

--- Sports Store/SportsStore/Models/BaseModel.cs
+++ Sports Store/SportsStore/Models/BaseModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace SportsStore.Models
 {
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+
     public class BaseModel<T>
     {
+        [BindNever]
         public T Id { get; set; }
     }
 }
