6a5032aac822d9f2f396ae40ce636a3ae3d5fef5
T3ch0o <trottimusbg@gmail.com>
2019-01-20 18:18:12 +0200

    [AdminController] Add Index action to display products and Edit action which return current product.

---
+++ Sports Store/SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿namespace SportsStore.Controllers
+{
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models;
+    using SportsStore.Services.Interfaces;
+
+    public class AdminController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_productService.GetAll());
+        }
+
+        public IActionResult Edit(int productId)
+        {
+            Product product = _productService.GetAll().FirstOrDefault(p => p.Id == productId);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
