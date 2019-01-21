30bb933a6a55863ed1e33e24f895b4fabe252e89
T3ch0o <trottimusbg@gmail.com>
2019-01-21 14:33:41 +0200

    [BaseModel] Remove BindNever attribute.

--- Sports Store/SportsStore/Models/BaseModel.cs
+++ Sports Store/SportsStore/Models/BaseModel.cs
@@ -1,10 +1,7 @@
 ﻿namespace SportsStore.Models
 {
-    using Microsoft.AspNetCore.Mvc.ModelBinding;
-
     public class BaseModel<T>
     {
-        [BindNever]
         public T Id { get; set; }
     }
 }
